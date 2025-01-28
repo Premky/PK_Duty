9966564a674ef55ab7ee4906add9402ba6bafdf7
Prem <www.prem.mass@gmail.com>
2025-01-28 22:28:55 +0545

    Added correction

--- Frontend/src/Components/Admin/Police/CountReport.jsx
+++ Frontend/src/Components/Admin/Police/CountReport.jsx
@@ -382,11 +382,46 @@ const CountPoliceReport = () => {
                                 <TableCell align="center" className='bg-secondary fw-bold'>जम्मा</TableCell>
                             </TableRow>
                         </TableHead>
+                        <TableBody>
+                            {records.map((record, index) => (
+                                <TableRow key={index}>
+                                    <TableCell align='center'>{index + 1}</TableCell>
+                                    <TableCell>{record.CaseNameNP}</TableCell>
+                                    <TableCell align='center'>{record.KaidiTotal}</TableCell>
+                                    <TableCell align='center'>{record.ThunuwaTotal}</TableCell>
+                                    <TableCell align='center'>{parseInt(record.Nabalak) + parseInt(record.Nabalika)}</TableCell>
+                                    <TableCell align='center' className='fw-bold'>{parseInt(record.ThunuwaTotal) + parseInt(record.KaidiTotal)}</TableCell>
+                                    <TableCell align='center'>{record.KaidiMale}</TableCell>
+                                    <TableCell align='center'>{record.KaidiFemale}</TableCell>
+                                    <TableCell align='center' className='fw-bold'>{parseInt(record.KaidiMale) + parseInt(record.KaidiFemale)}</TableCell>
+                                    <TableCell align='center'>{record.ThunuwaMale}</TableCell>
+                                    <TableCell align='center'>{record.ThunuwaFemale}</TableCell>
+                                    <TableCell align='center' className='fw-bold'>{parseInt(record.ThunuwaMale) + parseInt(record.ThunuwaFemale)}</TableCell>
 
-                        {/* Suspense to show a fallback while the body is loading */}
+                                    <TableCell align='center'>{record.TotalArrestedInDateRange}</TableCell>
+                                    <TableCell align='center'>{record.TotalReleasedInDateRange}</TableCell>
+                                </TableRow>
+                            ))}
+                            <TableRow key='total' >
+                                <TableCell className='bg-primary fw-bold' colSpan={2}>जम्मा</TableCell>
+                                <TableCell align='center' className='bg-success fw-bold'>{totals.KaidiTotal}</TableCell>
+                                <TableCell align='center' className='bg-success fw-bold'>{totals.ThunuwaTotal}</TableCell>
+                                <TableCell align='center' className='bg-success fw-bold'>{parseInt(totals.Nabalak) + parseInt(totals.Nabalika)}</TableCell>
+                                <TableCell align='center' className='bg-success fw-bold'>{parseInt(totals.KaidiTotal) + parseInt(totals.ThunuwaTotal) + parseInt(totals.Nabalak) + parseInt(totals.Nabalika)}</TableCell>
+                                <TableCell align='center' className='bg-success fw-bold'>{totals.KaidiMale}</TableCell>
+                                <TableCell align='center' className='bg-success fw-bold'>{totals.KaidiFemale}</TableCell>
+                                <TableCell align='center' className='bg-success fw-bold'>{parseInt(totals.KaidiMale) + parseInt(totals.KaidiFemale)}</TableCell>
+                                <TableCell align='center' className='bg-success fw-bold'>{totals.ThunuwaMale}</TableCell>
+                                <TableCell align='center' className='bg-success fw-bold'>{totals.ThunuwaFemale}</TableCell>
+                                <TableCell align='center' className='bg-success fw-bold'>{parseInt(totals.ThunuwaMale) + parseInt(totals.ThunuwaFemale)}</TableCell>
+                                <TableCell align='center' className='bg-success fw-bold'>{totals.SumOfArrestedInDateRange}</TableCell>
+                                <TableCell align='center' className='bg-success fw-bold'>{totals.SumOfReleasedInDateRange}</TableCell>
+                            </TableRow>
+                        </TableBody>
+                        {/* Suspense to show a fallback while the body is loading
                         <Suspense fallback={<div>Loading...</div>}>
                             {!isLoading && <LazyTableBody records={records} totals={totals} />}
-                        </Suspense>
+                        </Suspense> */}
                     </Table>
                 </TableContainer>
 
--- Frontend/src/Components/Admin/Police/CountTableBody.jsx
+++ Frontend/src/Components/Admin/Police/CountTableBody.jsx
@@ -4,45 +4,45 @@ import {
   FormControl, Container, Select, InputLabel,
   MenuItem
 } from '@mui/material';
-const CountTableBody = ({records,totals}) => {
+const CountTableBody = ({ records, totals }) => {
   return (
     <>
-    <TableBody>
-                            {records.map((record, index) => (
-                                <TableRow key={index}>
-                                    <TableCell align='center'>{index + 1}</TableCell>
-                                    <TableCell>{record.CaseNameNP}</TableCell>
-                                    <TableCell align='center'>{record.KaidiTotal}</TableCell>
-                                    <TableCell align='center'>{record.ThunuwaTotal}</TableCell>
-                                    <TableCell align='center'>{parseInt(record.Nabalak) + parseInt(record.Nabalika)}</TableCell>
-                                    <TableCell align='center' className='fw-bold'>{parseInt(record.ThunuwaTotal) + parseInt(record.KaidiTotal)}</TableCell>
-                                    <TableCell align='center'>{record.KaidiMale}</TableCell>
-                                    <TableCell align='center'>{record.KaidiFemale}</TableCell>
-                                    <TableCell align='center' className='fw-bold'>{parseInt(record.KaidiMale) + parseInt(record.KaidiFemale)}</TableCell>
-                                    <TableCell align='center'>{record.ThunuwaMale}</TableCell>
-                                    <TableCell align='center'>{record.ThunuwaFemale}</TableCell>
-                                    <TableCell align='center' className='fw-bold'>{parseInt(record.ThunuwaMale) + parseInt(record.ThunuwaFemale)}</TableCell>
+      <TableBody>
+        {records.map((record, index) => (
+          <TableRow key={index}>
+            <TableCell align='center'>{index + 1}</TableCell>
+            <TableCell>{record.CaseNameNP}</TableCell>
+            <TableCell align='center'>{record.KaidiTotal}</TableCell>
+            <TableCell align='center'>{record.ThunuwaTotal}</TableCell>
+            <TableCell align='center'>{parseInt(record.Nabalak) + parseInt(record.Nabalika)}</TableCell>
+            <TableCell align='center' className='fw-bold'>{parseInt(record.ThunuwaTotal) + parseInt(record.KaidiTotal)}</TableCell>
+            <TableCell align='center'>{record.KaidiMale}</TableCell>
+            <TableCell align='center'>{record.KaidiFemale}</TableCell>
+            <TableCell align='center' className='fw-bold'>{parseInt(record.KaidiMale) + parseInt(record.KaidiFemale)}</TableCell>
+            <TableCell align='center'>{record.ThunuwaMale}</TableCell>
+            <TableCell align='center'>{record.ThunuwaFemale}</TableCell>
+            <TableCell align='center' className='fw-bold'>{parseInt(record.ThunuwaMale) + parseInt(record.ThunuwaFemale)}</TableCell>
 
-                                    <TableCell align='center'>{record.TotalArrestedInDateRange}</TableCell>
-                                    <TableCell align='center'>{record.TotalReleasedInDateRange}</TableCell>
-                                </TableRow>
-                            ))}
-                            <TableRow key='total' >
-                                <TableCell className='bg-primary fw-bold' colSpan={2}>जम्मा</TableCell>
-                                <TableCell align='center' className='bg-success fw-bold'>{totals.KaidiTotal}</TableCell>
-                                <TableCell align='center' className='bg-success fw-bold'>{totals.ThunuwaTotal}</TableCell>
-                                <TableCell align='center' className='bg-success fw-bold'>{parseInt(totals.Nabalak) + parseInt(totals.Nabalika)}</TableCell>
-                                <TableCell align='center' className='bg-success fw-bold'>{parseInt(totals.KaidiTotal) + parseInt(totals.ThunuwaTotal) + parseInt(totals.Nabalak) + parseInt(totals.Nabalika)}</TableCell>
-                                <TableCell align='center' className='bg-success fw-bold'>{totals.KaidiMale}</TableCell>
-                                <TableCell align='center' className='bg-success fw-bold'>{totals.KaidiFemale}</TableCell>
-                                <TableCell align='center' className='bg-success fw-bold'>{parseInt(totals.KaidiMale) + parseInt(totals.KaidiFemale)}</TableCell>
-                                <TableCell align='center' className='bg-success fw-bold'>{totals.ThunuwaMale}</TableCell>
-                                <TableCell align='center' className='bg-success fw-bold'>{totals.ThunuwaFemale}</TableCell>
-                                <TableCell align='center' className='bg-success fw-bold'>{parseInt(totals.ThunuwaMale) + parseInt(totals.ThunuwaFemale)}</TableCell>
-                                <TableCell align='center' className='bg-success fw-bold'>{totals.SumOfArrestedInDateRange}</TableCell>
-                                <TableCell align='center' className='bg-success fw-bold'>{totals.SumOfReleasedInDateRange}</TableCell>
-                            </TableRow>
-                        </TableBody>
+            <TableCell align='center'>{record.TotalArrestedInDateRange}</TableCell>
+            <TableCell align='center'>{record.TotalReleasedInDateRange}</TableCell>
+          </TableRow>
+        ))}
+        <TableRow key='total' >
+          <TableCell className='bg-primary fw-bold' colSpan={2}>जम्मा</TableCell>
+          <TableCell align='center' className='bg-success fw-bold'>{totals.KaidiTotal}</TableCell>
+          <TableCell align='center' className='bg-success fw-bold'>{totals.ThunuwaTotal}</TableCell>
+          <TableCell align='center' className='bg-success fw-bold'>{parseInt(totals.Nabalak) + parseInt(totals.Nabalika)}</TableCell>
+          <TableCell align='center' className='bg-success fw-bold'>{parseInt(totals.KaidiTotal) + parseInt(totals.ThunuwaTotal) + parseInt(totals.Nabalak) + parseInt(totals.Nabalika)}</TableCell>
+          <TableCell align='center' className='bg-success fw-bold'>{totals.KaidiMale}</TableCell>
+          <TableCell align='center' className='bg-success fw-bold'>{totals.KaidiFemale}</TableCell>
+          <TableCell align='center' className='bg-success fw-bold'>{parseInt(totals.KaidiMale) + parseInt(totals.KaidiFemale)}</TableCell>
+          <TableCell align='center' className='bg-success fw-bold'>{totals.ThunuwaMale}</TableCell>
+          <TableCell align='center' className='bg-success fw-bold'>{totals.ThunuwaFemale}</TableCell>
+          <TableCell align='center' className='bg-success fw-bold'>{parseInt(totals.ThunuwaMale) + parseInt(totals.ThunuwaFemale)}</TableCell>
+          <TableCell align='center' className='bg-success fw-bold'>{totals.SumOfArrestedInDateRange}</TableCell>
+          <TableCell align='center' className='bg-success fw-bold'>{totals.SumOfReleasedInDateRange}</TableCell>
+        </TableRow>
+      </TableBody>
     </>
   )
 }
